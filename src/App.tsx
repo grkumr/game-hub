a59ccb8e99bfd719609e8bc8225a03eaa515480a
grkumr <grmk2023@gmail.com>
2024-07-06 11:40:11 +0530

    Implement searching

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchText: string;
 }
 
 function App() {
@@ -28,7 +29,7 @@ function App() {
       lg: '200px 1fr'
     }}
   >
-    <GridItem area='nav'><Navbar /></GridItem>
+    <GridItem area='nav'><Navbar onSearch={(searchText) => setGameQuery({...gameQuery, searchText})}/></GridItem>
     <Show above='lg'><GridItem area='aside' paddingX={7}>
       <GenreList selectedGenre={gameQuery.genre} onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })} />
     </GridItem> </Show>
