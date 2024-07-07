cda4b12b64e19f0c8ba675c1b81681b61f20d1a9
grkumr <grmk2023@gmail.com>
2024-07-07 10:12:54 +0530

    Refractor gamegrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,9 +14,10 @@ const GameGrid = ({ gameQery }: Props) => {
   const { data, error, isLoading } = useGames(gameQery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
+  if (error) return <Text>{error}</Text>
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
+    
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6} padding="10px">
         {isLoading &&
           skeletons.map((skeleton) => (
@@ -30,7 +31,7 @@ const GameGrid = ({ gameQery }: Props) => {
           </GameCardContainer>
         ))}
       </SimpleGrid>
-    </>
+    
   );
 };
 
